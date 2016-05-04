38e02519387057a7438aca2e44962e721b7bea6f
Huberto Kaiser Filho <betinhoh@gmail.com>
2016-05-04 10:54:14 -0300

    Fixing view of recovering password

--- application/views/senha.php
+++ application/views/senha.php
@@ -1,14 +1,48 @@
-<div class="contentInicio center">
+<section class="login">
+	<div class="container">
 		<form class="formLogin" name="formLogin" action="<?php echo base_url();?>index.php/CI_login/recuperar" method="post">
-			<h3>RECUPERAR SENHA</h3>
-		    <div class="labels">
-				<label for="email">E-mail</label>
-		    </div>    
-		    <div class="campos">
-				<input type="text" name="email" class="inputs" />
-		    </div>    
-		    <input type="submit" value="Recuperar" class="botao"/>
-		    <div class="clear"><!-- --></div>
-		    <p><a class="btn-recover" href="<?php echo base_url(); ?>index.php/CI_login/" title="Voltar ao menu inicial">Volar ao menu inicial!</a></p>
+			<div class="row">
+				<div class="col-sm-4 col-sm-offset-4">
+					<div class="login-box">
+						<div class="row">
+							<div class="col-xs-12">
+								<h2>RECUPERAR SENHA</h2>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xs-10 col-xs-offset-1">
+								<label for="email">E-mail</label>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xs-10 col-xs-offset-1">
+								<input type="text" name="email" class="inputs" />
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xs-10 col-xs-offset-1">
+								<div class="erro-login-msg">
+									<?php echo @$msg; ?>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="submit">
+								<div class="col-md-6 col-md-offset-3">
+									<input type="submit" value="Recuperar" class="botao"/>
+								</div>			
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-6 col-md-offset-3">
+								<div class="pass-recover">
+									<a class="btn-recover" href="<?php echo base_url(); ?>index.php/CI_login/" title="Voltar ao menu inicial">Volar ao menu inicial</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</form>
-</div>
+	</div>
+</section>
