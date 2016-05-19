fd0e62935df2a57e3f47594b84c3c77f2c0ae9ec
Huberto Kaiser Filho <betinhoh@gmail.com>
2016-05-19 11:30:33 -0300

    Modified part of the welcome message

--- application/views/saudacao.php
+++ application/views/saudacao.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row">
     		<div class="col-xs-12">
-    			<h3>Bem vindo <?php echo $usuario_logado;?>, use o menu superior para navegar pelo Gerenciador.</h3>
+    			<h3>Bem vindo <?php echo $usuario_logado;?>, use o menu superior para navegar.</h3>
 				<!-- Clique <a href="CI_about">aqui</a> para mais detalhes do sistema. -->
     		</div>
 		</div>
