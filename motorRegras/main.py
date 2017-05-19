99a6789c4f1a7a3685cfc127930adac43bf5ba68
Huberto Kaiser Filho <betinhoh@gmail.com>
2017-05-19 12:03:23 -0300

    changed obj parameters on may

--- motorRegras/main.py
+++ motorRegras/main.py
@@ -12,8 +12,8 @@ try:
     obj_parameters = sys.argv[2] # recebe o nome da regra
     rules          = json.load(sys.argv[1]) # recebe a regra e extrai a regra do json
     condiction_satisfied = run_all(rule_list=rule,
-                defined_variables=ConditionsRules(json.load),
-                defined_actions=ActionRules(json.load),
+                defined_variables=ConditionsRules(obj_parameters),
+                defined_actions=ActionRules(obj_parameters),
                 stop_on_first_trigger=True
                 )
     print(condiction_satisfied)
