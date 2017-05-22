d3e23cc4035483bd269d6f9bb86837145602e72d
Huberto Kaiser Filho <betinhoh@gmail.com>
2017-05-22 11:21:18 -0300

    getting email params from db through api

--- motorRegras/ActionRules.py
+++ motorRegras/ActionRules.py
@@ -20,12 +20,16 @@ import smtplib
 class ActionRules(BaseActions):
     def __init__ (self, parameters):
         self.name = parameters
-
+        headers = {'Content-type': 'application/json', 'X-API-KEY': 'cfb281929c3574091ad2a7cf80274421e6a87c59'}
+        url = "http://localhost/{0}/api/configuracoes".format(os.path.relpath(".",".."))       
+        r = requests.get(url, headers=headers)
+        initial_list = json.loads(r.json())
+        self.configs = dict((item['name'], item) for item in initial_list)
 
     @rule_action(params = {"email": FIELD_TEXT})
-    def send_email(self, email):
+    def send_email(self, email):        
 
-        sender = 'teste@teste.com.br'
+        sender = self.configs['email_from']['value']
         receivers = [email]
 
         message = "Valores fora das condições estipuladas na regra {0}".format(self.name)
@@ -39,13 +43,12 @@ class ActionRules(BaseActions):
         msg['Subject'] = subject
         msg.attach(MIMEText(message))
         try:
-            smtpObj = smtplib.SMTP('smtp.gmail.com',587)
+            smtpObj = smtplib.SMTP(self.configs['email_host']['value'],self.configs['email_port']['value'])
             smtpObj.ehlo()
             smtpObj.starttls()
-            smtpObj.login(sender,"3123123123121")
+            smtpObj.login(self.configs['email_user']['value'],self.configs['email_pass']['value'])
             smtpObj.sendmail(sender, receivers, msg.as_string())
             print ("Successfully sent email")
             smtpObj.quit()
         except :
             print ("Error: unable to send email")
-            smtpObj.quit()
